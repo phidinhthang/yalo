6f7298b56957d5382187c41ab20febd6e34cffd4
phidinhthang <phidinhthang1112@gmail.com>
2022-04-02 11:47:44 +0700

    fix(server): disable foreign keys to fix migration up for non superuser

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -32,6 +32,7 @@ const config: Options = {
   migrations: {
     path: 'dist/common/migrations',
     pathTs: 'src/common/migrations',
+    disableForeignKeys: false,
   },
   driverOptions:
     process.env.NODE_ENV === 'production'
